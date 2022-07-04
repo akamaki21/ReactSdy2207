7005a6ee44fb0169ca312a82466a6d356729ba69
akamaki21 <84122175+akamaki21@users.noreply.github.com>
2022-07-04 10:35:00 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App1 = () => {
+  return (
+    <>
+      <h1> ２回目のReact </h1>
+      <p> まだまだ覚えられんから何度もやる</p>
+    </>
+  );
+};
+
+export default App1;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App1 from "./App";
+
+ReactDom.render(<App1 />, document.getElementById("root"));
